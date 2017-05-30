f0de8741d510220abc953d385d7e5cda667413d7
shahin@sheidaei.com <shahinsheidaei@Peregrine.local>
2017-05-30 15:07:13 -0400

    hashed password

--- user/config.php
+++ user/config.php
@@ -67,7 +67,7 @@ define( 'YOURLS_COOKIEKEY', 'modify this text with something random' );
  ** YOURLS will auto encrypt plain text passwords in this file
  ** Read http://yourls.org/userpassword for more information */
 $yourls_user_passwords = array(
-	'username' => 'password',
+	'shahin' => 'a45927812c079d3d5fef38ba483a3dc0',
 	// 'username2' => 'password2',
 	// You can have one or more 'login'=>'password' lines
 	);
